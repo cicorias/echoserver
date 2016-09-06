f000a57c814a52a1fef128a9b2afa2d8e3c3801e
cicorias <github@cicoria.com>
2016-09-06 16:01:12 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Siple Echo and Client
+This is just a test tool that will loop and ping a server that has the signature GET /name/parm...
+
+# server
+Built using Restify
+
+
+# Client
+built using Restify's JsonClient
---
+++ client.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict';
+var argv = require('yargs').argv;
+
+if (! argv.url ){
+  console.log('you need a URL parm :  --url=http://foobar')
+  return;
+}
+
+if (! argv.loopCount ){
+  console.log('you need a loopCount parm :  --loopCount=3600    - this would be 1 our of pings')
+  return;
+}
+
+var restify = require('restify');
+var assert = require('assert');
+
+var url =  argv.url;
+
+var client = restify.createJsonClient({
+  url: url,
+  accept: 'application/json',
+  requestTimeout: 30,
+  version: '*'
+});
+
+var loopCounter = 0;
+var loopCount = argv.loopCount;
+var looper;
+
+function loopit() {
+  if (++loopCounter > loopCount) {
+    clearInterval(looper);
+  }
+  else {
+    client.get('/hello/joe', function (err, req, res, data) {
+      assert.ifError(err);
+      console.log('count: %s: %j', loopCounter, data);
+    });
+  }
+
+}
+
+looper = setInterval(loopit, 1000);
+
